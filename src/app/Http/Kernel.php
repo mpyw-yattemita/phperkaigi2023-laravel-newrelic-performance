842dc7140282058072e2574bc91c2da20d6e9e73
mpyw <mpyw628@gmail.com>
2023-03-16 12:11:24 +0900

    Upgrade to Laravel 10 (#6)

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -32,11 +32,11 @@ class Kernel extends HttpKernel
     ];
 
     /**
-     * The application's route middleware.
+     * The application's middleware aliases.
      *
-     * These middleware may be assigned to groups or used individually.
+     * Aliases may be used to conveniently assign middleware to routes and groups.
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [];
+    protected $middlewareAliases = [];
 }
